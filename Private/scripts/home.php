4340b58e38cfe30bcea67da2fa754ea9d7f380a4
Dan <my25mb@has.im>
2026-01-26 12:16:52 +0100

    Update home.php

--- Private/scripts/home.php
+++ Private/scripts/home.php
@@ -485,9 +485,9 @@
 <?PHP endif; ?>
 
   <meta name="description" content="Welcome to Buzzcing! Let everyone have its listings."/>
-  <meta name="keywords" content="buzzcing,on,premise,solution"/>
+  <meta name="keywords" content="buzzcing,on,premise,solution,NuMode"/>
   <meta name="robots" content="index,follow"/>
-  <meta name="author" content="5 Mode"/>
+  <meta name="author" content="NuMode"/>
   
   <script src="/js/jquery-3.6.0.min.js" type="text/javascript"></script>
   <script src="/js/sha.js" type="text/javascript"></script>
@@ -567,7 +567,7 @@
  </form>   
            
   <div id="footerCont">&nbsp;</div>
-  <div id="footer"><span style="background:#FFFFFF; opacity:0.7;">&nbsp;&nbsp;A <a href="http://5mode.com" class="aaa">5 Mode</a> project and <a href="http://demo.5mode.com" class="aaa">WYSIWYG</a> system. <?PHP echo(getResource0("Some rights reserved", $lang));?></span></div>
+  <div id="footer"><span style="background:#FFFFFF; opacity:0.7;">&nbsp;&nbsp;A <a href="http://numode.eu" class="aaa">NuMode</a> project and <a href="http://demo.numode.eu" class="aaa">WYSIWYG</a> system. <?PHP echo(getResource0("Some rights reserved", $lang));?></span></div>
            
 <?PHP else: ?>          
 
